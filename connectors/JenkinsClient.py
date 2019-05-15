60268829389983208adc06391442ccecac8cc787
thomas-vogel444 <thomas.vogel@digital.hmrc.gov.uk>
2019-05-15 08:34:29 +0100

    Modularise my script

---
+++ connectors/JenkinsClient.py
@@ -0,0 +1,15 @@
+import requests
+import json
+
+class JenkinsPage:
+    def __init__(self, nodeDescription, numExecutors):
+        self.nodeDescription = nodeDescription
+        self.numExecutors = numExecutors
+
+    def __str__(self):
+        return "Description: %s, Number of Executors: %s" %(self.nodeDescription, self.numExecutors)
+
+class JenkinsClient:
+    def getJenkinsPage(self):
+        response = json.loads(requests.get("https://lab03.build.tax.service.gov.uk/api/json", auth=('thomas.vogel', 'Dfens1987!444')).content)
+        return JenkinsPage(response["nodeDescription"], response["numExecutors"])
